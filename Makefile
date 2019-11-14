244b8ae795a9c6827e10693592e3727527de4be1
Anthony Johnson <aj@ohess.org>
2019-11-13 19:25:37 -0700

    Add theme and fix fonts

--- Makefile
+++ Makefile
@@ -37,5 +37,5 @@ clean-convert:
 # Catch-all target: route all unknown targets to Sphinx using the new
 # "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
 #%: Makefile
-html: Makefile $(targets)
+clean html: Makefile $(targets)
 	$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
--- conf.py
+++ conf.py
@@ -10,9 +10,18 @@ extensions = [
 
 templates_path = ['_templates']
 html_static_path = ['_static']
+html_theme_path = ['_themes']
 
 # Generic options
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 
 # HTML theme options
-html_theme = 'alabaster'
+html_theme = 'microej'
+html_logo = '_themes/microej/static/logo-microej-white.png'
+# Until there is a more maintainable theme, override the fonts with an include
+html_css_files = ['fonts.css']
+
+# This is required because the version of the Read the Docs Sphinx theme the
+# microej theme was forked from does not support the HTML5 writer used by
+# default by Sphinx 2+
+html4_writer = True
