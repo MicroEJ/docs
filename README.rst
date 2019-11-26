7743e4d4312d12830e0fd2cf96972e0bc4b77eb0
Anthony Johnson <aj@ohess.org>
2019-11-26 14:08:59 -0700

    Some clean up and documentation

--- README.rst
+++ README.rst
@@ -1,1 +1,18 @@
+MicroEJ Documentation
+=====================
+
 This repository holds the official MicroEJ documentation available at https://docs.microej.com/.
+
+Components
+----------
+
+MicroEJ Sphinx theme
+    The theme includes some CSS overrides to the ``sphinx_rtd_theme`` and
+    additional font files. It is located at ``_themes/microej``.
+
+MicroEJ Sphinx extension
+    This extension is used to provider a Pygments style class, for defining
+    styles used for syntax highlighting, some customizations to Sphinx roles,
+    and the helpers used in ``conf.py`` to enable an optional build mode that
+    outputs independent HTML/PDF documentation sets. The module is located at
+    ``microej.py`` and includes documentation on each of the pieces.
--- conf.py
+++ conf.py
@@ -8,21 +8,18 @@ sys.path.insert(0, os.path.dirname(__file__))
 import microej
 
 
-project = 'MicroEJ'
+project = 'MicroEJ Documentation'
 copyright = '2019, MicroEJ'
 author = 'MicroEJ'
 release = '1.0'
 
-# Conditional docset
-docset = os.environ.get('MICROEJ_DOCSET', None)
-if docset == 'SandboxedAppDevGuide':
-    project = 'Sandboxed Application Developer Guide'
-elif docset == 'StandaloneAppDevGuide':
-    project = 'Standalone Application Developer Guide'
-elif docset == 'ApplicationDeveloperGuide':
-    project = 'Application Developer Guide'
-elif docset == 'Landing':
-    project = 'MicroEJ Documentation'
+# This isn't executed on normal builds, but can be used to build multiple doc
+# sets from this common source. See the ``microej.py`` extension for more
+# information.
+if microej.can_build_independent_docs():
+    project_name = microej.get_project_name()
+    if project_name:
+        project = project_name
 
 extensions = [
     'microej',
@@ -60,17 +57,6 @@ latex_elements = {
 latex_additional_files = ['microej.sty']
 latex_logo = '_themes/microej/static/mascot-xl.png'
 
-# This isn't picked up from the theme, and is required to get the LaTeX
-# pygments bridge working
+# This is defined in the theme, but the LaTeX builder does not honor the theme
+# setting for pygments. This is the same style class used by the HTML builder.
 pygments_style = 'microej.MicroEJStyle'
-
-
-def setup(app):
-    # Because there are multiple, interconnected docsets in these sources,
-    # there will be a number of errors about duplicate section labels, and
-    # links will not resolve as expected. This allows for multiple, independent
-    # projects in the same doc source.
-    # TODO if/when this project is restructured as a single document, without
-    # included sections between the docs, this likely won't be need.
-    if docset:
-        app.srcdir += '/' + docset
