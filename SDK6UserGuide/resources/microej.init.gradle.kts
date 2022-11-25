8d22f51dab36470d97d6e0b506ff400242e48685
Thomas Delhoménie <thomas.delhomenie@microej.com>
2022-11-25 11:58:22 +0100

    M0078DDG-514 Improve default repositories

---
+++ SDK6UserGuide/resources/microej.init.gradle.kts
@@ -0,0 +1,106 @@
+settingsEvaluated {
+
+   allprojects {
+      repositories {
+         /* MicroEJ Central repository for Maven/Gradle modules */
+         maven {
+             name = "microEJCentral"
+             url = uri("https://repository.microej.com/5/artifacts/")
+         }
+         /* MicroEJ Forge Central repository for Maven/Gradle modules */
+         maven {
+             name = "microEJForgeCentral"
+             url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+         }
+         /* MicroEJ Forge Developer repository for Maven/Gradle modules */
+         maven {
+             name = "microEJForgeDeveloper"
+             url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+         }
+         /* MicroEJ Central repository for Ivy modules */
+         ivy {
+             name = "microEJCentralIvy"
+             url = uri("https://repository.microej.com/5/artifacts/")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+         /* MicroEJ Forge Central repository for Ivy modules */
+         ivy {
+             name = "microEJForgeCentralIvy"
+             url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+         /* MicroEJ Forge Developer repository for Ivy modules */
+         ivy {
+             name = "microEJForgeDeveloperIvy"
+             url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+      }
+   }
+
+
+   /**
+    * Plugins repositories
+    */
+   pluginManagement {
+      repositories {
+         /* MicroEJ Central repository for Maven/Gradle modules */
+         maven {
+             name = "microEJCentral"
+             url = uri("https://repository.microej.com/5/artifacts/")
+         }
+         /* MicroEJ Forge Central repository for Maven/Gradle modules */
+         maven {
+             name = "microEJForgeCentral"
+             url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+         }
+         /* MicroEJ Forge Developer repository for Maven/Gradle modules */
+         maven {
+             name = "microEJForgeDeveloper"
+             url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+         }
+         /* MicroEJ Central repository for Ivy modules */
+         ivy {
+             name = "microEJCentralIvy"
+             url = uri("https://repository.microej.com/5/artifacts/")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+         /* MicroEJ Forge Central repository for Ivy modules */
+         ivy {
+             name = "microEJForgeCentralIvy"
+             url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+         /* MicroEJ Forge Developer repository for Ivy modules */
+         ivy {
+             name = "microEJForgeDeveloperIvy"
+             url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+      }
+   }
+}
