4ea63aef2d1edf31a89a0d242aea0d1c9a244fff
Thomas Delhoménie <thomas.delhomenie@microej.com>
2022-12-09 16:51:59 +0100

    M0078DDG-514 Add Forge SDK 6 repository in Gradle init script

--- SDK6UserGuide/resources/microej.init.gradle.kts
+++ SDK6UserGuide/resources/microej.init.gradle.kts
@@ -71,6 +71,11 @@ settingsEvaluated {
              name = "microEJForgeDeveloper"
              url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
          }
+         /* MicroEJ Forge SDK 6 repository for Maven/Gradle modules */
+         maven {
+             name = "microEJForgeSDK6"
+             url = uri("https://forge.microej.com/artifactory/microej-sdk6-repository-release/")
+         }
          /* MicroEJ Central repository for Ivy modules */
          ivy {
              name = "microEJCentralIvy"
@@ -95,6 +100,16 @@ settingsEvaluated {
          ivy {
              name = "microEJForgeDeveloperIvy"
              url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+         /* MicroEJ Forge SDK 6 repository for Ivy modules */
+         ivy {
+             name = "microEJForgeSDK6Ivy"
+             url = uri("https://forge.microej.com/artifactory/microej-sdk6-repository-release/")
              patternLayout {
                  artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
                  ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
