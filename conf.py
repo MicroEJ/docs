0a546f1dc02352d36cb8a47d478f718798de6134
Rémy LOUEDEC <remy.louedec@microej.com>
2023-10-23 10:57:35 -0400

    Add linkcheck ignore for 'forum.segger.com': it does not provide its CA certificates

--- conf.py
+++ conf.py
@@ -88,6 +88,7 @@ pygments_style = 'microej.MicroEJStyle'
 #   - local index links flagged as broken by linkcheck. Hopefully temporary solution 
 #     until https://github.com/sphinx-doc/sphinx/issues/9383 is resolved.
 #   - unstable URLs which make the link check fail, such as www.gnu.org or www.oracle.com
-linkcheck_ignore = [r'^((?!:\/\/|#|@).)*$|^https://www\.gnu\.org/software/gettext/manual/.*$|^https://www.oracle.com/.*$']
+#   - https://forum.segger.com does not provide its CA certificates
+linkcheck_ignore = [r'^((?!:\/\/|#|@).)*$|^https://www\.gnu\.org/software/gettext/manual/.*$|^https://www.oracle.com/.*$|^https://forum.segger.com/.*$']
 
 linkcheck_timeout = 20
