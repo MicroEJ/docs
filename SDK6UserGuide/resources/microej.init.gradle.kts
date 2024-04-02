4f350c32963a8de58778c9621e8e5997bbcd98df
Thomas Delhoménie <thomas.delhomenie@microej.com>
2024-04-02 15:09:57 +0200

    M0090IDE-4669 Add Central repository in SDK 6 configuration

--- SDK6UserGuide/resources/microej.init.gradle.kts
+++ SDK6UserGuide/resources/microej.init.gradle.kts
@@ -21,6 +21,11 @@ settingsEvaluated {
          }
          /* MicroEJ Central repository for Maven/Gradle modules */
          maven {
+             name = "microEJCentral"
+             url = uri("https://repository.microej.com/modules")
+         }
+         /* MicroEJ Forge Central repository for Maven/Gradle modules */
+         maven {
              name = "microEJForgeCentral"
              url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
          }
@@ -35,6 +40,16 @@ settingsEvaluated {
              url = uri("https://forge.microej.com/artifactory/microej-sdk6-repository-release/")
          }
          /* MicroEJ Central repository for Ivy modules */
+         ivy {
+             name = "microEJCentralIvy"
+             url = uri("https://repository.microej.com/modules")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+         /* MicroEJ Forge Central repository for Ivy modules */
          ivy {
              name = "microEJForgeCentralIvy"
              url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
@@ -88,6 +103,11 @@ settingsEvaluated {
    pluginManagement {
       repositories {
          /* MicroEJ Central repository for Maven/Gradle modules */
+        maven {
+             name = "microEJCentral"
+             url = uri("https://repository.microej.com/modules")
+         }
+         /* MicroEJ Forge Central repository for Maven/Gradle modules */
          maven {
              name = "microEJForgeCentral"
              url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
@@ -103,6 +123,16 @@ settingsEvaluated {
              url = uri("https://forge.microej.com/artifactory/microej-sdk6-repository-release/")
          }
          /* MicroEJ Central repository for Ivy modules */
+         ivy {
+             name = "microEJCentralIvy"
+             url = uri("https://repository.microej.com/modules")
+             patternLayout {
+                 artifact("[organisation]/[module]/[revision]/[artifact]-[revision](-[classifier])(.[ext])")
+                 ivy("[organisation]/[module]/[revision]/ivy-[revision].xml")
+                 setM2compatible(true)
+             }
+         }
+         /* MicroEJ Forge Central repository for Ivy modules */
          ivy {
              name = "microEJForgeCentralIvy"
              url = uri("https://forge.microej.com/artifactory/microej-central-repository-release")
