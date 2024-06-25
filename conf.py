2afc6ba71783bb92eebe46c65a08fbd18ac11410
Félix Pinel <felix.pinel@microej.com>
2024-06-25 12:49:08 +0200

    keep old linkcheck_ignore patterns commented for occasional testing

--- conf.py
+++ conf.py
@@ -83,10 +83,18 @@ latex_engine = 'lualatex'
 # setting for pygments. This is the same style class used by the HTML builder.
 pygments_style = 'microej.MicroEJStyle'
 
-# ignoring Github links with anchors at linkcheck
-# linkcheck_ignore = [r'https?:\/\/github\.com\/.+#.+']
-
 # Have linkcheck ignore all links except those containing "microej.com" (main website, docs, developer, repository, forge...) or starting with a "#" (inner references)
 linkcheck_ignore = [r'^(?!#|.*microej\.com)']
+
+# Keep these legacy linkcheck_ignore patterns for occasional monitoring of external links 
+# Ignore some links:
+#   - local index links flagged as broken by linkcheck. Hopefully temporary solution
+#     until https://github.com/sphinx-doc/sphinx/issues/9383 is resolved.
+#   - unstable URLs which make the link check fail, such as www.gnu.org or www.oracle.com
+#   - https://forum.segger.com does not provide its CA certificates
+#   - https://site.mockito.org DNS failure (2024/02/12)
+#linkcheck_ignore = [r'^((?!:\/\/|#|@).)*$|^https://www\.gnu\.org/software/gettext/manual/.*$|^http://localhost.*$|^http://172.17.0.1.*$|^https://www.oracle.com/.*$|^https://forum.segger.com/.*$|^https://site.mockito.org.*$|^https://helpx.adobe.com/illustrator/using/simplify_paths.html$']
+# ignoring Github links with anchors at linkcheck
+#linkcheck_ignore = [r'https?:\/\/github\.com\/.+#.+']
 
 linkcheck_timeout = 20
